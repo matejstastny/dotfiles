724e17c4e87f52014931368089cd724415ecaeac
Matej Stastny <matysta@outlook.com>
2025-09-12 13:53:13 -0700

    Added global config script

---
+++ scripts/config.sh
@@ -0,0 +1,11 @@
+ARCH=$(uname -m)
+OS_TYPE="$(uname)"
+
+DOTFILES_DIR="$HOME/dotfiles"
+ASSETS_DIR="$DOTFILES_DIR/assets"
+FONT_DIR="$ASSETS_DIR/fonts"
+CONFIGS_DIR="$DOTFILES_DIR/configs"
+WALLPAPER_DIR="$ASSETS_DIR/wallpapers"
+
+BREWFILE="$DOTFILES_DIR/Brewfile"
+WALLPAPER="$WALLPAPER_DIR/mac-wallpaper.jpg"
