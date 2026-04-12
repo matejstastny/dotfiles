0e58234fb1111c0a7c94959776c8f0021923e6d3
Matej Stastny <matysta@outlook.com>
2026-04-11 23:53:31 -0700

    Added dot python req to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,16 +3,30 @@ set -eu
 
 DOTFILES_DIR="$HOME/dotfiles"
 
+# Install Homebrewwww :3
 if ! command -v brew >/dev/null 2>&1; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
+# Python for my cushtom dot script
+PYTHON=$(command -v python3 || true)
+PYTHON_OK=false
+if [ -n "$PYTHON" ]; then
+	version=$("$PYTHON" -c 'import sys; print(sys.version_info >= (3, 11))' 2>/dev/null || echo False)
+	[ "$version" = "True" ] && PYTHON_OK=true
+fi
+if [ "$PYTHON_OK" = false ]; then
+	brew install python@3.11
+fi
+
+# Uhh.. the main thing the dotfiles :D
 if [ ! -d "$DOTFILES_DIR" ]; then
-    git clone https://github.com/matejstastny/dotfiles.git "$DOTFILES_DIR"
+	git clone https://github.com/matejstastny/dotfiles.git "$DOTFILES_DIR"
 fi
 
+# Its already executable, but just in case loll
 chmod +x "$DOTFILES_DIR/bin/dot"
 "$DOTFILES_DIR/bin/dot" all
 
-echo "Done. Restart your terminal."
+echo "Done. Restart your terminal!"
