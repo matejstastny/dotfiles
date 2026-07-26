9ea9dd058a813ab3d93ae039e25121c9f9681043
Matej Stastny <matysta@outlook.com>
2026-07-26 11:45:46 -0700

    hyprpm slop

--- configs/hypr/hyprland.lua
+++ configs/hypr/hyprland.lua
@@ -10,6 +10,9 @@ DOTS = HOME .. "/dotfiles"
 local dir = HOME .. "/.config/hypr/modules/"
 local function load(m) dofile(dir .. m .. ".lua") end
 
+-- cause of hyprmp slop loading
+hl.config({ debug = { suppress_errors = true } })
+
 load("env")
 load("monitors")
 load("look")
